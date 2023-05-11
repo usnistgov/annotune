dfb06e5fbef8f9ad62665ce305267ccdd55eab4e
Daniel Stephens <danystevo@gmail.com>
2023-05-11 04:08:15 -0400

    first bugs fixed

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ from xml.dom import minidom
 import requests
 import time
 import os
+from flask import jsonify
+import ast
 
 
 received_data = json.load(open('dataset.json'))
@@ -18,11 +20,8 @@ os.urandom(24).hex()
 
 topic_list = json.load(open('topic_list.json'))
 all_texts = json.load(open("newsgroup_sub_500.json"))
-counter=0
 base = "http://54.87.190.90//recommend_document"
-# results = get_texts(topic_list=topic_list, all_texts=all_texts)
-# response=json.load(open('dataset.json'))
-# response=None
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.urandom(24).hex()
@@ -38,19 +37,19 @@ def login():
     if request.method =="POST":
         name = request.form["name"]
         session["name"] = name
-        user = requests.post("http://54.87.190.90//create_user", {"user_session": session["name"]})
+        user = requests.post("https://nist-topic-model.umiacs.umd.edu//create_user", {"user_session": session["name"]})
 
-        id = user.json()["user_id"]
-        session["id"] = id
-        return redirect(url_for("home_page", name=name, id=id))
+        user_id = user.json()["user_id"]
+        session["user_id"] = user_id
+        return redirect(url_for("home_page", name=name, user_id=user_id))
     return render_template("login.html")
 
 
 
 
 # READING WHAT THE STUDY IS ABOUT
-@app.route("/firstpage/<name>/<id>", methods = ["POST", "GET"])
-def home_page(name, id):
+@app.route("/firstpage/<name>/<user_id>", methods = ["POST", "GET"])
+def home_page(name, user_id):
     # print(session)
     if session.get("name") != name:
         # if not there in the session then redirect to the login page
@@ -88,45 +87,34 @@ def topic_page(name):
         get_topic_list = "http://54.87.190.90//get_topic_list"
         # print(session)
         topics = requests.post(get_topic_list, json={
-                            "user_id": session["id"]
+                            "user_id": session["user_id"]
                             }).json()
+
         results = get_texts(topic_list=topics, all_texts=all_texts)
 
         if request.method =="POST":
             return redirect(url_for("finish"))
  
-        return render_template("state1.html", results=results, name=name, counter=counter)
+        return render_template("state1.html", results=results, name=name)
     return render_template("login.html")
 
 
 
 ### lABELLING THE TOPIC AND SAVING THE RESPONSE
-@app.route("/get_label/<id>/<counter>", methods=["POST", 'GET'])
-def get_label(id, counter):
-    counter=int(counter)
-    print(counter)
-    id = id
-    text = all_texts["text"][str(id)]
-    name=session["name"] 
-    recommend_document = "http://54.87.190.90//recommend_document"
 
-    if int(counter) == 0:
-        data = requests.post(recommend_document, json={
-            "document_id": "-1",
-            "label": '3',
-            "response_time": '1',
-            "user_id":session["id"]
-            }).json()
-        counter+=1
-        words = get_words(data["topic"], text)
 
-        return render_template("label.html", response=data, text=text, words=words )
+
+@app.route("/get_label/<document_id>/", methods=["POST", 'GET'])
+def get_label(document_id):
+    document_id = document_id 
+    user_id=session["user_id"] 
+    get_document_information = "https://nist-topic-model.umiacs.umd.edu/get_document_information"
+    data = requests.post(get_document_information, json={ "document_id": document_id,
+                                                        "user_id":user_id
+                                                         }).json()
+
+    return redirect( url_for("label", response=data, name =session["name"], document_id=document_id))
     
-    else:
-
-    
-        return render_template("try.html")
-    # return redirect(url_for("label", text=text, name=name, document_id=id, response=response))
 
 
 
@@ -148,120 +136,87 @@ def require_login():
 
 
 
-@app.route('/label/<name>/<int:document_id>/<response>', methods=['POST', 'GET'])
+@app.route('/label/<name>/<document_id>/<response>/', methods=['POST', 'GET'])
 def label(name,document_id, response):
     
-    # words = get_words(response["topic"], response["raw_text"])
     st = time.time()
+    response = ast.literal_eval(response)
+
+
+
+    print(response)
+    text = all_texts["text"][str(document_id)]
 
     if request.method =="POST":
         name=name
         document_id=document_id
-        user_id = session["id"]
+        user_id = session["user_id"]
         et = time.time()
         response_time = st- et
         label = request.form.get("label")
 
         save_response(name, label, response_time, document_id, user_id)
-        
-        labelling = {
-            "label":label,
-            "response_time": response_time,
-            "document_id": document_id,
-            "user_id" : user_id
-        }
-        recommend_document = "http://54.87.190.90//recommend_document"
-        data = requests.post(recommend_document, json=labelling)
-
     
-        response=data.json()
 
         return redirect(url_for("topic_page", name=name, response=response))
 
-    # text = response["raw_text"]
+    words = get_words(response["topic"],  text)
 
-    words = get_words(response["topic"], response["raw_text"])
-
-    return render_template("label.html", response=response, words=words, id=response["document_id"])
+    return render_template("label.html", response=response, words=words, document_id=document_id, text=text)
 
 
 
 
 
-# def submit_response():
+
+
+# @app.route('/', methods=['POST', 'GET'])
+# def get_data():
+#     global response 
+#     global counter
+#     id = response["document_id"]
+#     new_label = None
+#     label=None
+#     st = time.time()
+#     et=time.time()
+#     text = response["raw_text"]
+#     # word_dict = get_words(response["topic"], response["raw_text"])
+#     # text = highlight_words(text, word_dict["8"])
+#     words = get_words(response["topic"], response["raw_text"])
 
 #     if request.method=="POST":
+
+        
 #         et = time.time()
-#         id = received_data["doc_id"]
-#         if request.form.get("all_topics"):
-#             label = request.form.get("all_topics")
-#         else:
-#             label = None
-#         if request.form.get("label"):
-#             new_label = request.form.get("label")
-#         else:
-#             new_label = None
+#         # label = str(request.form["all_topics"])
+#         new_label = request.form["newlabel"]
 #         response_time = et -st
-
-#         response = {
-#             "doc_id": id, "label": label, "new_label": new_label, "response_time" : response_time
-#         }
-#         print(response)
-
-#         return response
-#     return render_template("index.html")
-
-
-
-@app.route('/', methods=['POST', 'GET'])
-def get_data():
-    global response 
-    global counter
-    id = response["document_id"]
-    new_label = None
-    label=None
-    st = time.time()
-    et=time.time()
-    text = response["raw_text"]
-    # word_dict = get_words(response["topic"], response["raw_text"])
-    # text = highlight_words(text, word_dict["8"])
-    words = get_words(response["topic"], response["raw_text"])
-
-    if request.method=="POST":
-
-        
-        et = time.time()
-        # label = str(request.form["all_topics"])
-        new_label = request.form["newlabel"]
-        response_time = et -st
         
 
-        if label and new_label:
-            flash('You cannot select an old label and a new label!')
+#         if label and new_label:
+#             flash('You cannot select an old label and a new label!')
 
 
-        elif not label and not new_label:
-            flash('Please select a label or enter a new label!')
+#         elif not label and not new_label:
+#             flash('Please select a label or enter a new label!')
 
 
-        else:
-            if len(new_label) == 0:
-                new_label ="None"
+#         else:
+#             if len(new_label) == 0:
+#                 new_label ="None"
         
-            if len(str(label))==0:
-                label = "None"
+#             if len(str(label))==0:
+#                 label = "None"
 
-            response = requests.post(base, json={"document_id": str(id), "label": label, "response_time": str(response_time)}).json()
-            text = response["raw_text"]
+#             response = requests.post(base, json={"document_id": str(id), "label": label, "response_time": str(response_time)}).json()
+#             text = response["raw_text"]
 
-            print(response_time)
-
-            counter +=1
+#             print(response_time)
 
             
-            return render_template("index.html", text=text, received_data=response, words=words, counter=counter)
+#             return render_template("index.html", text=text, received_data=response, words=words, counter=counter)
                 
-    return render_template("index.html", text=text, received_data=response, words=words, counter=counter)
+#     return render_template("index.html", text=text, received_data=response, words=words, counter=counter)
     
 
 
--- testtttt.py
+++ testtttt.py
@@ -20,83 +20,83 @@ import pandas as pd
 #     return words
 # received_data = json.load(open('newsgroup_sub_500.json'))
 
-df = pd.read_json("newsgroup_sub_500.json")
-print(df.head())
+# df = pd.read_json("newsgroup_sub_500.json")
+# print(df.head())
 
 # create_user = "http://54.87.190.90//create_user"
-# recommend_document = "http://54.87.190.90//recommend_document"
+# # recommend_document = "http://54.87.190.90//recommend_document"
 
-# # response=received_data
-# st = time.time()
+# # # response=received_data
+# # st = time.time()
 
-# user = requests.post(create_user, {
-#     "user_session": "daniel"
-#     })
-# print(user.json())
+# # user = requests.post(create_user, {
+# #     "user_session": "daniel"
+# #     })
+# # print(user.json())
 
-# response = requests.post(recommend_document, json={
-#     "document_id": '408',
-#     "label": '3',
-#     "response_time": '1',
-#     "user_id": 23
-# })
-# # print(user.json()["user_id"])
-# et = time.time()
+# # response = requests.post(recommend_document, json={
+# #     "document_id": '408',
+# #     "label": '3',
+# #     "response_time": '1',
+# #     "user_id": 23
+# # })
+# # # print(user.json()["user_id"])
+# # et = time.time()
 
-# elapsed_time = et - st
-# print(elapsed_time)
-# data = response.json()
-# print(data)
-# print(response.json())
-# # print(response.json()["topic_order"])
+# # elapsed_time = et - st
+# # print(elapsed_time)
+# # data = response.json()
+# # print(data)
+# # print(response.json())
+# # # print(response.json()["topic_order"])
 
-# # print(response.json()["topic"])
+# # # print(response.json()["topic"])
 
-# print(words("alt.atheism", data))
-# print(response.json())
-# print(respons)
-# 
-# print(get_words(data["topic"], data["raw_text"]))
-# responses = []
+# # print(words("alt.atheism", data))
+# # print(response.json())
+# # print(respons)
+# # 
+# # print(get_words(data["topic"], data["raw_text"]))
+# # responses = []
 
 
-def save_response(name, label, response_time, document_id, user_id):
-    root = minidom.Document()
-    xml = root.createElement('root')
-    root.appendChild(xml)
+# def save_response(name, label, response_time, document_id, user_id):
+#     root = minidom.Document()
+#     xml = root.createElement('root')
+#     root.appendChild(xml)
 
-    user_name = root.createElement('name')
-    user_name.setAttribute("name", name)
-    xml.appendChild(user_name)
+#     user_name = root.createElement('name')
+#     user_name.setAttribute("name", name)
+#     xml.appendChild(user_name)
 
-    response_times = root.createElement('response_time')
-    response_times.setAttribute("response_time", response_time)
-    xml.appendChild(response_times)
+#     response_times = root.createElement('response_time')
+#     response_times.setAttribute("response_time", response_time)
+#     xml.appendChild(response_times)
 
-    document_ids = root.createElement('document_id')
-    document_ids.setAttribute("document_id", document_id)
-    xml.appendChild(document_ids)
+#     document_ids = root.createElement('document_id')
+#     document_ids.setAttribute("document_id", document_id)
+#     xml.appendChild(document_ids)
 
-    labels = root.createElement('label')
-    labels.setAttribute("label", label)
-    xml.appendChild(labels)
+#     labels = root.createElement('label')
+#     labels.setAttribute("label", label)
+#     xml.appendChild(labels)
 
-    user_ids = root.createElement('user_id')
-    user_ids.setAttribute("user_id", user_id)
-    xml.appendChild(user_ids)
+#     user_ids = root.createElement('user_id')
+#     user_ids.setAttribute("user_id", user_id)
+#     xml.appendChild(user_ids)
 
-    xml_str = root.toprettyxml(indent ="\t")
-    directory = "./static/"+name
+#     xml_str = root.toprettyxml(indent ="\t")
+#     directory = "./static/"+name
 
-    save_path_file = directory + "/"+ str(document_id) +".xml"
-    try:
-        os.makedirs(name)
-    except:
-        print("all_good")
-    with open(save_path_file, "w") as f:
-        f.write(xml_str)
+#     save_path_file = directory + "/"+ str(document_id) +".xml"
+#     try:
+#         os.makedirs(name)
+#     except:
+#         print("all_good")
+#     with open(save_path_file, "w") as f:
+#         f.write(xml_str)
 
-    return xml_str
+#     return xml_str
 
 # print(save_response("daniel", "sports", "0.55", "1", "12"))
 
