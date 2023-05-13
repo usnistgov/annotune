375451884da10c64c06b689711a314d05ff4d659
Daniel Stephens <danystevo@gmail.com>
2023-05-13 01:01:13 -0400

    pages fixed

--- app.py
+++ app.py
@@ -58,8 +58,8 @@ def login():
 
 
 # READING WHAT THE STUDY IS ABOUT
-@app.route("/firstpage/<name>/<user_id>", methods = ["POST", "GET"])
-def home_page(name, user_id):
+@app.route("//firstpage//<name>/", methods = ["POST", "GET"])
+def home_page(name):
     # print(session)
     if session.get("name") != name:
         # if not there in the session then redirect to the login page
@@ -67,46 +67,111 @@ def home_page(name, user_id):
 
     if request.method =="POST":
 
-        return redirect(url_for("topic_page", name=name))
+        return redirect(url_for("active_check", name=name))
         
     return render_template("first.html", name=name)
 
 
-  
-# @app.route("/initial", methods=["POST", "GET"])
-# def initial_request():
-#     recommend_document = "http://54.87.190.90//recommend_document"
-#     data = requests.post(recommend_document, json={
-#             "document_id": "-1",
-#             "label": '3',
-#             "response_time": '1',
-#             "user_id":session["id"]
-#             })
-#     # print(data.json())
 
-#     global response 
-#     response=data.json()
-#     return redirect(url_for('topic_page', name=session['name'], response=response))
+@app.route("//checkactive//<name>//", methods=["POST", 'GET'])
+def active_check(name):
+    if session.get("name") != name:
+        # if not there in the session then redirect to the login page
+        return redirect("/login")
 
-
-## lIST OF TOPICS BASED ON THE SELECTED LIST
-@app.route("/topic/<name>", methods = ["POST", "GET"])
-def topic_page(name):
-    
-    if "name" in session:
-        get_topic_list = url + "//get_topic_list"
+    get_topic_list = url + "//get_topic_list"
         # print(session)
-        topics = requests.post(get_topic_list, json={
+    topics = requests.post(get_topic_list, json={
                             "user_id": session['user_id']
                             }).json()
 
-        results = get_texts(topic_list=topics, all_texts=all_texts)
+    print(len(topics["cluster"].keys()))
+    # print(topics)  
+    if len(topics["cluster"].keys()) == 1:
 
-        if request.method =="POST":
-            return redirect(url_for("finish"))
+        return redirect(url_for("active_list", name=name))
+
+    else:
+        return redirect(url_for("non_active_list", name=name))
+
+
+
+
+
+
+
+@app.route("//active_list//<name>", methods=["POST", "GET"])
+def active_list(name):
+    if session.get("name") != name:
+    # if not there in the session then redirect to the login page
+        return redirect("/login")
+
+    get_topic_list = url + "//get_topic_list"
+        # print(session)
+    topics = requests.post(get_topic_list, json={
+                            "user_id": session['user_id']
+                            }).json()
+    # print(topics)
+
+    results = get_texts(topic_list=topics, all_texts=all_texts)
+
+    if request.method =="POST":
+        return redirect(url_for("finish"))
  
-        return render_template("state1.html", results=results, name=name)
-    return render_template("login.html")
+    return render_template("active_list.html", results=results, name=name)
+
+    
+
+
+
+@app.route("//non_active_list//<name>", methods=["POST", "GET"])
+def non_active_list(name):
+    if session.get("name") != name:
+        # if not there in the session then redirect to the login page
+        return redirect("/login")
+
+    get_topic_list = url + "//get_topic_list"
+        # print(session)
+    topics = requests.post(get_topic_list, json={
+                            "user_id": session['user_id']
+                            }).json()
+
+    results = get_texts(topic_list=topics, all_texts=all_texts)
+
+    if request.method =="POST":
+        return redirect(url_for("finish"))
+
+    return render_template("nonactive.html", results=results, name=name)
+
+
+
+@app.route("//acitve//<name>//<document_id>", methods=["GET", "POST"])
+def active(name, document_id):
+    get_topic_list = url + "//get_topic_list"
+
+    topics = requests.post(get_topic_list, json={
+                            "user_id": session['user_id']
+                            }).json()
+
+    results = get_texts(topic_list=topics, all_texts=all_texts)
+    text = all_texts["text"][str(document_id)]
+    st =time.time()
+    if request.method =="POST":
+        name=name
+        document_id=document_id
+        user_id = session["user_id"]
+        et = time.time()
+        response_time = st- et
+        label = request.form.get("label")
+
+        save_response(name, label, response_time, document_id, user_id)
+    
+
+        return redirect(url_for("active_list", name=name))
+    return render_template("activelearning.html", text =text ) 
+
+    
+
 
 
 
@@ -114,7 +179,7 @@ def topic_page(name):
 
 
 
-@app.route("/get_label/<document_id>/", methods=["POST", 'GET'])
+@app.route("//get_label//<document_id>//", methods=["POST", 'GET'])
 def get_label(document_id):
     document_id = document_id 
     user_id=session["user_id"] 
@@ -122,7 +187,8 @@ def get_label(document_id):
     data = requests.post(get_document_information, json={ "document_id": document_id,
                                                         "user_id":user_id
                                                          }).json()
-    print(session)
+                                                         
+
 
     return redirect( url_for("label", response=data, name =session["name"], document_id=document_id))
     
@@ -133,7 +199,7 @@ def get_label(document_id):
 @app.route("/logout", methods=["POST", "GET"])
 def finish():
     name = session['name']
-    session.pop(name)
+    session.pop(name, None)
     return redirect(url_for("login"))
 
 
@@ -146,17 +212,16 @@ def require_login():
 
 
 
-
-@app.route('/label/<name>/<document_id>/<response>/', methods=['POST', 'GET'])
-def label(name,document_id, response):
-    
+@app.route('//non_active_label//<name>//<document_id>', methods=["POST", "GET"])
+def non_active_label(name, document_id):
     st = time.time()
-    response = ast.literal_eval(response)
+    get_document_information = url + "//get_document_information"
+    response = requests.post(get_document_information, json={ "document_id": document_id,
+                                                        "user_id":session["user_id"]
+                                                         }).json()
 
-
-
-    print(response)
     text = all_texts["text"][str(document_id)]
+    words = get_words(response["topic"],  text)
 
     if request.method =="POST":
         name=name
@@ -169,65 +234,9 @@ def label(name,document_id, response):
         save_response(name, label, response_time, document_id, user_id)
     
 
-        return redirect(url_for("topic_page", name=name, response=response))
-
-    words = get_words(response["topic"],  text)
-
-    return render_template("label.html", response=response, words=words, document_id=document_id, text=text)
+        return redirect(url_for("non_active_list", name=name))
 
 
-
-
-
-
-
-# @app.route('/', methods=['POST', 'GET'])
-# def get_data():
-#     global response 
-#     global counter
-#     id = response["document_id"]
-#     new_label = None
-#     label=None
-#     st = time.time()
-#     et=time.time()
-#     text = response["raw_text"]
-#     # word_dict = get_words(response["topic"], response["raw_text"])
-#     # text = highlight_words(text, word_dict["8"])
-#     words = get_words(response["topic"], response["raw_text"])
-
-#     if request.method=="POST":
-
-        
-#         et = time.time()
-#         # label = str(request.form["all_topics"])
-#         new_label = request.form["newlabel"]
-#         response_time = et -st
-        
-
-#         if label and new_label:
-#             flash('You cannot select an old label and a new label!')
-
-
-#         elif not label and not new_label:
-#             flash('Please select a label or enter a new label!')
-
-
-#         else:
-#             if len(new_label) == 0:
-#                 new_label ="None"
-        
-#             if len(str(label))==0:
-#                 label = "None"
-
-#             response = requests.post(base, json={"document_id": str(id), "label": label, "response_time": str(response_time)}).json()
-#             text = response["raw_text"]
-
-#             print(response_time)
-
-            
-#             return render_template("index.html", text=text, received_data=response, words=words, counter=counter)
-                
-#     return render_template("index.html", text=text, received_data=response, words=words, counter=counter)
-    
+    return render_template("nonactivelabel.html", response=response, words=words, document_id=document_id, text=text)
 
 
